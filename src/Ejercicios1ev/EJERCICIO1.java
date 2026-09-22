550bbd69e9d8830c48debec4a26460c027ce1e94
Cesar <Tarde@A32P26>
2026-09-22 20:06:52 +0200

    Ejercicio1

---
+++ src/Ejercicios1ev/EJERCICIO1.java
@@ -0,0 +1,15 @@
+package Ejercicios1ev;
+
+public class EJERCICIO1 {
+
+	public static void main(String[] args) {
+		int a = -9, b = 21, c = -35, d = 51, e = 33, f;
+
+		int resultado = (f = (a + b) * e) / (d % c);
+		
+		System.out.println ("El resultado es: " + resultado);
+		
+
+	}
+
+}
